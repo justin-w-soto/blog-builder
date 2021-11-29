ecd76948feed4fe8b515969f8fa87fcc6fec8519
Justin W Soto <soto@pdx.edu>
2021-11-29 14:05:28 -0800

    turned off prettier

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,6 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
-
+import { useState } from 'react'
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
 
@@ -7,11 +8,27 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState()
+  const [text, setText] = useState()
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
+      <Preview 
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        font={font}
+        setFont={setFont}
+        align={align}
+        setAlign={setAlign}
+        text={text}
+        setText={setText}
+      />
       <Editor />
     </main>
   )
