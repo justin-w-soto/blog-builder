ec56123ebb1e23934e8e4b09b00ce22720e405a0
Justin W Soto <soto@pdx.edu>
2021-11-29 14:25:18 -0800

    app is functioning

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,11 +8,11 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('')
-  const [subtitle, setSubtitle] = useState('')
+  const [title, setTitle] = useState('Hello!')
+  const [subtitle, setSubtitle] = useState('Create Your First Blog')
   const [font, setFont] = useState('')
-  const [align, setAlign] = useState()
-  const [text, setText] = useState()
+  const [align, setAlign] = useState('center')
+  const [text, setText] = useState('')
 
   return (
     <main>
@@ -29,7 +29,17 @@ export default function Home() {
         text={text}
         setText={setText}
       />
-      <Editor />
+      <Editor     
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        font={font}
+        setFont={setFont}
+        align={align}
+        setAlign={setAlign}
+        text={text}
+        setText={setText} />
     </main>
   )
 }
