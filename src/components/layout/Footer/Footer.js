ef5aec7e8368fe49ee6475b55c565f787108ae52
Justin W Soto <soto@pdx.edu>
2021-11-29 15:21:46 -0800

    fixed prettier issue for good

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react'
 import './Footer.css'
 export default function Footer() {
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react'
 import { useState } from 'react'
 import Preview from '../../components/Preview/Preview'
@@ -19,15 +18,10 @@ export default function Home() {
       {/* pass the state variables as props to the presentational components */}
       <Preview 
         title={title}
-        setTitle={setTitle}
         subtitle={subtitle}
-        setSubtitle={setSubtitle}
         font={font}
-        setFont={setFont}
         align={align}
-        setAlign={setAlign}
         text={text}
-        setText={setText}
       />
       <Editor     
         title={title}
